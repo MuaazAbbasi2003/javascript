3e4a15c5c0ed6491c862777477be7842e06a3cb4
Muaaz Abbasi <muaazabbasi2003@gmail.com>
2025-08-26 19:36:49 +0500

    made some chnages in js foundation  commneted

--- JS-Foundation/JS-Foundation.js
+++ JS-Foundation/JS-Foundation.js
@@ -6,11 +6,53 @@
 //   console.log(count);
 // }
 
-let teaCollection = [];
-let tea = "";
-do {
-  tea = prompt(`enter your favorite tea type (Enter"stop to stop the loop")`);
-  if (tea !== "stop") {
-    teaCollection.push(tea);
-  }
-} while (tea !== "stop");
+// let teaCollection = [];
+// let tea = "";
+// do {
+//   tea = prompt(`enter your favorite tea type (Enter"stop to stop the loop")`);
+//   if (tea !== "stop") {
+//     teaCollection.push(tea);
+//   }
+// } while (tea !== "stop");
+// let sum = 0;
+// let i = 1;
+
+// do {
+//   sum += i;
+//   console.log(i);
+//   i++;
+// } while (i <= 4);
+// console.log(sum);
+// let arr1 = [2, 4, 6, 8, 10];
+// let arr2 = [];
+// for (let i = 0; i < arr1.length; i++) {
+//   x = arr1[i];
+//   console.log(x);
+//   if (x === 8) {
+//     continue;
+//   }
+//   arr2.push(x);
+// }
+// console.log(arr2);
+
+// let arr1 = [1, 2, 3, 3, 4, 4, 4];
+// for (const i of arr1) {
+//   console.log(i);
+// }
+
+// let worldCities = {
+//   "Sydney-name": 5000000,
+//   Tokyo: 9000000,
+//   Berlin: 3500000,
+//   Paris: 2200000,
+// };
+// let newObject = {};
+// for (const key in worldCities) {
+//   if (worldCities[key] > 3000000) {
+//     newObject[key] = worldCities[key];
+//     console.log("hello");
+//   } else {
+//     continue;
+//   }
+// }
+// console.log(newObject);
