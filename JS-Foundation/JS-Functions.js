2494588cb3a32d7765fc489ea9eb41247fd2b553
Muaaz Abbasi <muaazabbasi2003@gmail.com>
2025-08-26 20:26:55 +0500

    new

---
+++ JS-Foundation/JS-Functions.js
@@ -0,0 +1,13 @@
+// function myName(obj) {
+//   return obj();
+// }
+
+// myName(() => {
+//   console.log("function inside a function");
+
+//   function innerFunction() {
+//     console.log("this is a nested function inside the callback");
+//   }
+
+//   innerFunction();
+// });
